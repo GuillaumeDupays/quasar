475ef6442e8dfc2143eeff912db037ec30d15241
GuillaumeDupays <guillaumedupays@gmail.com>
2021-01-02 21:27:43 +0100

    tester le framework quasar.js

---
+++ quasar-app/src/pages/Index.vue
@@ -0,0 +1,49 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-table
+      title="Fichiers"
+      :data="data"
+      :columns="columns"
+      row-key="filename"
+    />
+  </q-page>
+</template>
+
+<script>
+import { format } from 'quasar';
+const { humanStorageSize } = format;
+
+export default {
+  data () {
+    return {
+      columns: [
+        {
+          name: 'filename',
+          label: 'Nom du fichier',
+          align: 'left',
+          field: row => row.filename,
+          sortable: true
+        },
+        {
+          name: 'storageSize',
+          label: 'Poids du fichier',
+          field: row => row.storageSize,
+          format: val => val ? humanStorageSize(val) : val,
+          sortable: true
+        }
+      ],
+      data: [
+        {
+          filename: 'readme.md',
+          storageSize: 15000
+        },
+        {
+          filename: 'demo.avi',
+          storageSize: 23700000
+        }
+      ]
+    }
+  }
+
+}
+</script>
